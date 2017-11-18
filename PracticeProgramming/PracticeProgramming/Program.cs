edf103e325af4568d766175ec1337d676a46a3b9
Sukh Dol <sdol@linus7.com>
2017-11-18 06:59:41 -0700

    Solved - Project Euler - Smallest Multiple problem

--- PracticeProgramming/PracticeProgramming/Program.cs
+++ PracticeProgramming/PracticeProgramming/Program.cs
@@ -6,7 +6,7 @@ namespace PracticeProgramming
     {
         static void Main(string[] args)
         {
-            LargestPalindromeProduct.SolveProblem();
+            SmallestMultiple.SolveProblem();
         }
     }
 }
---
+++ PracticeProgramming/PracticeProgramming/ProjectEuler/SmallestMultiple.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PracticeProgramming.ProjectEuler
+{
+    public static class SmallestMultiple
+    {
+        public static void SolveProblem()
+        {
+            // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+            // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+
+            const int minNumber = 1;
+            const int maxNumber = 20;
+            var counter = maxNumber;
+            var tempCounter = 0;
+            bool isDivisible;
+            isDivisible = false;
+
+            while (!isDivisible)
+            {
+                counter++;
+
+                for (var i = minNumber; i <= maxNumber; i++)
+                {
+                    tempCounter = i;
+                    if (counter % i != 0)
+                    {
+                        break;
+                    }
+                }
+
+                if (tempCounter == maxNumber)
+                {
+                    Console.WriteLine(counter);
+                    isDivisible = true;
+                }
+            }
+        }
+
+    }
+}
